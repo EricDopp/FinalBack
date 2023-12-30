c495085ec139f49cda19b4afa9619d572e26b588
Justin Willow <justinjwillow@gmail.com>
2023-12-29 16:36:38 -0800

    Added workout API functionality

---
+++ Final/Controllers/WorkoutSetsController.cs
@@ -0,0 +1,103 @@
+﻿using Final.Models;
+using Final.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Final.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WorkoutSetsController : ControllerBase
+    {
+        private readonly IWorkoutSetsService _workoutSetsService;
+
+        public WorkoutSetsController(IWorkoutSetsService workoutSetsService)
+        {
+            _workoutSetsService = workoutSetsService;
+        }
+
+        [HttpPost]
+        public IActionResult CreateWorkoutSets([FromBody] WorkoutSets workoutSets)
+        {
+            try
+            {
+                var createdWorkoutSet = _workoutSetsService.CreateWorkoutSets(
+                    workoutSets.UserId,
+                    workoutSets.WorkoutPlanId,
+                    workoutSets.ExerciseDBId,
+                    workoutSets.Weight,
+                    workoutSets.RepCount,
+                    workoutSets.WorkoutSetCount,
+                    workoutSets.WeightUnit,
+                    workoutSets.SortOrder
+                );
+
+                return Ok(createdWorkoutSet);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Outer exception: {ex.Message}");
+
+                if (ex.InnerException != null)
+                {
+                    Console.WriteLine($"Inner exception: {ex.InnerException.Message}");
+                }
+                throw;
+            }
+        }
+
+        [HttpGet("{userId}")]
+        public IActionResult GetWorkoutSetsByUserId(string userId)
+        {
+            try
+            {
+                var workoutSets = _workoutSetsService.GetWorkoutSetsByUserId(userId);
+                return Ok(workoutSets);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{workoutSetId}")]
+        public IActionResult UpdateWorkoutSets(int workoutSetId, [FromBody] WorkoutSets workoutSets)
+        {
+            try
+            {
+                var updatedWorkoutSet = _workoutSetsService.UpdateWorkoutSets(
+                    workoutSetId,
+                    workoutSets.Weight,
+                    workoutSets.RepCount,
+                    workoutSets.WorkoutSetCount,
+                    workoutSets.SortOrder
+                );
+
+                if (updatedWorkoutSet != null)
+                    return Ok(updatedWorkoutSet);
+                else
+                    return NotFound();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{workoutSetId}")]
+        public IActionResult DeleteWorkoutSets(int workoutSetId)
+        {
+            try
+            {
+                var result = _workoutSetsService.DeleteWorkoutSets(workoutSetId);
+                if (result)
+                    return Ok();
+                else
+                    return NotFound();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ Final/Services/WorkoutSetsService.cs
@@ -0,0 +1,74 @@
+﻿using Final.Data;
+using Final.Models;
+
+namespace Final.Services
+{
+    public class WorkoutSetsService : IWorkoutSetsService
+    {
+        private readonly AppDbContext _dbContext;
+
+        public WorkoutSetsService(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public WorkoutSets CreateWorkoutSets(string userId, int workoutPlanId, string exerciseDBId, decimal weight, int reps, int workoutSets, string weightUnit, int sortOrder)
+        {
+            var workoutSet = new WorkoutSets
+            {
+                UserId = userId,
+                WorkoutPlanId = workoutPlanId,
+                ExerciseDBId = exerciseDBId,
+                RepCount = reps,
+                Weight = weight,
+                WorkoutSetCount = workoutSets,
+                WeightUnit = weightUnit,
+                SortOrder = sortOrder
+            };
+
+            _dbContext.WorkoutSets.Add(workoutSet);
+            _dbContext.SaveChanges();
+
+            return workoutSet;
+        }
+
+        public List<WorkoutSets> GetWorkoutSetsByUserId(string userId)
+        {
+            return _dbContext.WorkoutSets
+                .Where(ws => ws.UserId == userId)
+                .OrderBy(ws => ws.SortOrder)
+                .ToList();
+        }
+
+        public WorkoutSets UpdateWorkoutSets(int workoutSetId, decimal weight, int reps, int workoutSets, int sortOrder)
+        {
+            var workoutSet = _dbContext.WorkoutSets.Find(workoutSetId);
+
+            if (workoutSet != null)
+            {
+                workoutSet.Weight = weight;
+                workoutSet.RepCount = reps;
+                workoutSet.WorkoutSetCount = workoutSets;
+                workoutSet.SortOrder = sortOrder;
+
+                _dbContext.SaveChanges();
+            }
+
+            return workoutSet;
+        }
+
+        public bool DeleteWorkoutSets(int workoutSetId)
+        {
+            var workoutSet = _dbContext.WorkoutSets.Find(workoutSetId);
+
+            if (workoutSet != null)
+            {
+                _dbContext.WorkoutSets.Remove(workoutSet);
+                _dbContext.SaveChanges();
+                return true;
+            }
+
+            return false;
+        }
+    }
+}

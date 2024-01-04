1a2559c7a96df118ac5cb7ca06f8ed0f369c2761
Eric Dopp <ericdopp@proton.me>
2024-01-03 21:59:35 -0500

    Submitting appsettings changes and migration history

---
+++ Final/Migrations/20240103021047_Initialization.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Final.Migrations
+{
+    public partial class Initialization : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WorkoutPlans",
+                columns: table => new
+                {
+                    WorkoutPlanId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WorkoutPlanName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    WeekDay = table.Column<string>(type: "nvarchar(13)", maxLength: 13, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkoutPlans", x => x.WorkoutPlanId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WorkoutSets",
+                columns: table => new
+                {
+                    WorkoutSetId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WorkoutPlanId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ExerciseDBId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RepCount = table.Column<int>(type: "int", nullable: false),
+                    Weight = table.Column<decimal>(type: "decimal(5,2)", nullable: false),
+                    WeightUnit = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    WorkoutSetCount = table.Column<int>(type: "int", nullable: false),
+                    SortOrder = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkoutSets", x => x.WorkoutSetId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WorkoutPlans");
+
+            migrationBuilder.DropTable(
+                name: "WorkoutSets");
+        }
+    }
+}

d8a6cca748ad188786f044a6c7d8dbc6e2e2b1dd
MichaelDSwopeVox <digitalwhiterabbithole@gmail.com>
2024-01-03 21:57:36 -0500

    readding contextdb

---
+++ Final/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Final.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Final.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<WorkoutPlan> WorkoutPlans { get; set; }
+    public DbSet<WorkoutSets> WorkoutSets { get; set; }
+}
